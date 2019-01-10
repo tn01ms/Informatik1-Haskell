7122d6bef8d614e74d4ba35bfb571f3101eb17e2
tn01ms <tn01ms@gmail.com>
2019-01-10 10:10:49 +0100

    Update verzahnen.hs
    Lists with same length work now

--- verzahnen.hs
+++ verzahnen.hs
@@ -1,5 +1,6 @@
 module Verzahnen where
 verzahnen [] [] = []
+verzahnen (x:[]) (y:[]) = (x:(y:[]))
 verzahnen [] (y:ys) = (y:verzahnen [] ys)
 verzahnen (x:xs) [] = (x:verzahnen [] xs)
 verzahnen (x:xs) (y:ys) = (x:(y:verzahnen xs ys))
