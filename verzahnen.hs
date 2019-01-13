9a5d342369740a8c5ff2ebfdeda8aaf1a38c5cea
tn01ms <tn01ms@gmail.com>
2019-01-13 19:41:19 +0100

    Delete verzahnen.hs

--- verzahnen.hs
+++
@@ -1,6 +0,0 @@
-module Verzahnen where
-verzahnen [] [] = []
-verzahnen (x:[]) (y:[]) = (x:(y:[]))
-verzahnen [] (y:ys) = (y:verzahnen [] ys)
-verzahnen (x:xs) [] = (x:verzahnen [] xs)
-verzahnen (x:xs) (y:ys) = (x:(y:verzahnen xs ys))
