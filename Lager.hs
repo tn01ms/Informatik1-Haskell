23df693626ed3fe144911e994976ca54fdef1e9c
tn01ms <tn01ms@gmail.com>
2019-01-17 16:57:36 +0100

    Update Lager.hs

--- Lager.hs
+++ Lager.hs
@@ -3,6 +3,8 @@ module Lager where
 import Data
 
 getID (id,_,_) = id
+getBezGew [(_, bez, gew)] = (bez, gew)
+getGew (_,_,gew) = gew
 
 leeresLager :: Lager
 leeresLager = []
@@ -10,6 +12,8 @@ leeresLager = []
 einlagern :: Int -> String -> Int -> Lager -> Lager
 einlagern id bez gew lag = [(id, bez, gew)] ++ lag
 
-suche :: Int -> Lager -> Int
-suche id lag = getID lag
---suche id lag = [elem|elem <- lag, (getID (head elem)) == id]
+suche :: Int -> Lager -> (String, Int)
+suche id lag = getBezGew [elem|elem <- lag, (getID elem) == id]
+
+sucheNachGewicht :: Int -> Int -> Lager -> Lager
+sucheNachGewicht gew1 gew2 lag = [elem|elem <- lag, (getGew elem) > gew1, (getGew elem) < gew2]
