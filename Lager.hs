058a7fcc0c0457b66bcbd157ddb45f82752494e8
tn01ms <tn01ms@gmail.com>
2019-01-17 17:02:24 +0100

    Update Lager.hs

--- Lager.hs
+++ Lager.hs
@@ -13,7 +13,7 @@ einlagern :: Int -> String -> Int -> Lager -> Lager
 einlagern id bez gew lag = [(id, bez, gew)] ++ lag
 
 suche :: Int -> Lager -> (String, Int)
-suche id lag = getBezGew [elem|elem <- lag, (getID elem) == id]
+suche id lag = if length ([elem|elem <- lag, (getID elem) == id]) > 0 then getBezGew [elem|elem <- lag, (getID elem) == id] else ("nicht gefunden", -1)
 
 sucheNachGewicht :: Int -> Int -> Lager -> Lager
-sucheNachGewicht gew1 gew2 lag = [elem|elem <- lag, (getGew elem) > gew1, (getGew elem) < gew2]
+sucheNachGewicht gew1 gew2 lag = [elem|elem <- lag, (getGew elem) >= gew1, (getGew elem) <= gew2]
