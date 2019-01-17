0aa452ce66ee7f64d6c636732948347f46e190b8
tn01ms <tn01ms@gmail.com>
2019-01-17 16:39:41 +0100

    Update Streckenzug.hs

--- Streckenzug.hs
+++ Streckenzug.hs
@@ -1,13 +1,30 @@
 module Streckenzug where
+import Punkt
+
+type Streckenzug = [Punkt]
+
+class Eq Double where
+   (==) :: Double -> Double -> Bool
+   (==) a b = if (fst a) == (fst b) then true else false
+   
+vergleiche :: a -> a -> Bool
+vergleiche a b = a == b
 
 -- TODO
 
+distanz :: Punkt -> Punkt -> Float
+distanz a b = sqrt (((fst a - fst b)^2) + ((snd a - snd b)^2))
+
+laenge :: Streckenzug -> Float
+laenge [] = 0.0
+laenge [(_,_)] = 0.0
+laenge (x:(y:ys)) = (distanz x y) + (laenge ([y]++ys))
 
 
 -- zum Anzeigen eines Punktes
-instance Show Punkt where
-  show (Punkt x y) = "(" ++ show x ++ "," ++ show y ++ ")"
+--instance Show Punkt where
+--  show (Punkt x y) = "(" ++ show x ++ "," ++ show y ++ ")"
   
 -- zum Anzeigen eines Streckenzugs
-instance Show Streckenzug where
-  show (Streckenzug x) = show x
+--instance Show Streckenzug where
+--  show (Streckenzug x) = show x
