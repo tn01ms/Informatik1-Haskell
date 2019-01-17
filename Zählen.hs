2b533a043c16b70a4c64e480044eeda7845cdee2
tn01ms <tn01ms@gmail.com>
2019-01-17 11:32:04 +0100

    Update Zählen.hs

--- Zählen.hs
+++ Zählen.hs
@@ -1,3 +1,9 @@
 zaehle :: [Int] -> Int -> Int
 
 zaehle list x = length [i|i <- list, i == x]
+
+zaehle2 :: [Int] -> Int -> Int -> Int
+zaehle2 [] y z = z
+zaehle2 (x:xs) y z
+        | x == y = zaehle2 xs y z++
+        | otherwise = zaehle2 xs y z
