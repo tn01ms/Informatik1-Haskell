593a121016ad5fcdd056d71fa03f278f36cb69cf
tn01ms <tn01ms@gmail.com>
2019-01-20 19:26:43 +0100

    Update Streckenzug.hs

--- Streckenzug.hs
+++ Streckenzug.hs
@@ -2,18 +2,9 @@ module Streckenzug where
 import Punkt
 
 type Streckenzug = [Punkt]
-
-instance Eq Punkt where
-     (a,b) == (c,d) = true
-
-
---   (==) :: Double -> Double -> Bool
---   (==) a b = if (fst a) == (fst b) then true else false
    
 vergleiche :: a -> a -> Bool
 vergleiche a b = a == b
-
--- TODO
 
 distanz :: Punkt -> Punkt -> Float
 distanz a b = sqrt (((fst a - fst b)^2) + ((snd a - snd b)^2))
@@ -22,6 +13,9 @@ laenge :: Streckenzug -> Float
 laenge [] = 0.0
 laenge [(_,_)] = 0.0
 laenge (x:(y:ys)) = (distanz x y) + (laenge ([y]++ys))
+
+
+
 
 
 -- zum Anzeigen eines Punktes
